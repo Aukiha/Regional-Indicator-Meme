91b24403e8b1565ec62956cc94f9c8003a24a664
Aukiha <aukihat@gmail.com>
2017-03-06 10:57:07 -0500

    retreating back to working code
    not allowing numbers caused a social and political uprising so we're going back to before

--- Regional Indicators.py
+++ Regional Indicators.py
@@ -1,35 +1,25 @@
 #Tkinter GUI coming to theatres near you
+#Just don't use numbers fam
+
 
 word = input()
 word = word.lower()
 word = list(word)
 counter = len(word)
-counter2 = len(word)
 position = 0
-newword = list()
-newpos = 0
-n = "0"
 
-while 2 == 2:
-        
-    while counter2 > 0:
-        if word[position] == n or "," or ".":
-            print("Numbers/Special Characters are not supported.")
-            break
-        else:
-            counter2 = counter2 - 1
-            temp = word[position]
-            position = position + 1
-    while counter > 0:
-        if word[position] == " ":
-            counter = counter - 1
-            word[position] = (" ")
-            temp = word[position]
-            position = position + 1
-        else:
-            counter = counter - 1
-            temp = word[position]
-            word[position] = (":regional_indicator_" + temp + ": ")
-            position = position + 1
+
+while counter > 0:
+    if word[position] == " ":
+        counter = counter - 1
+        word[position] = (" ")
+        temp = word[position]
+        position = position + 1
+    else:
+        counter = counter - 1
+        temp = word[position]
+        word[position] = (":regional_indicator_" + temp + ": ")
+        position = position + 1
+counter =- 1
 
 print("".join(word))
