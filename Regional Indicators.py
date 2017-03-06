de0af71f56478d48c63ad885388fabdedaa30b9e
Aukiha <aukihat@gmail.com>
2017-03-06 11:33:16 -0500

    Update with test GUI
    it works okay

--- Regional Indicators.py
+++ Regional Indicators.py
@@ -1,25 +1,41 @@
 #Tkinter GUI coming to theatres near you
 #Just don't use numbers fam
 
+from tkinter import *
+master = Tk()
 
-word = input()
-word = word.lower()
-word = list(word)
-counter = len(word)
-position = 0
+text = Entry(master, width=50)
+text.pack()
+
+text.focus_set()
+
+def callback():
+    word = text.get()
+    word = word.lower()
+    word = list(word)
+    counter = len(word)
+    position = 0
 
 
-while counter > 0:
-    if word[position] == " ":
-        counter = counter - 1
-        word[position] = (" ")
-        temp = word[position]
-        position = position + 1
-    else:
-        counter = counter - 1
-        temp = word[position]
-        word[position] = (":regional_indicator_" + temp + ": ")
-        position = position + 1
-counter =- 1
+    while counter > 0:
+        if word[position] == " ":
+            counter = counter - 1
+            word[position] = (" ")
+            temp = word[position]
+            position = position + 1
+        else:
+            counter = counter - 1
+            temp = word[position]
+            word[position] = (":regional_indicator_" + temp + ": ")
+            position = position + 1
+    counter =- 1
 
-print("".join(word))
+    word = "".join(word)
+    master.clipboard_append(word)
+    messagebox.showinfo("Conversion Complete", "Copied to clipboard")
+
+b = Button(master, text="Convert to Regional Indicators", width=30, command=callback)
+b.pack()
+
+mainloop()
+
