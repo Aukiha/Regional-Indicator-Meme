f01e4acfa674f4d93ccfa1793b8aac170aae8af9
Aukiha <aukihat@gmail.com>
2017-04-19 17:50:04 -0400

    Update main script
    why isnt this an .exe file yet

--- Regional Indicators.py
+++ Regional Indicators.py
@@ -1,3 +1,4 @@
+
 from tkinter import *
 
 #Initilizes Tkinter window
@@ -24,45 +25,65 @@ def callback():
             word[position] = (" ")
             temp = word[position]
             position = position + 1
+        elif word[position] == "b":
+            counter = counter - 1
+            word[position] = (":b: ")
+            position = position + 1
         elif word[position] == "1":
             counter = counter - 1
-            word[position] = (":one:")
+            word[position] = (":one: ")
             position = position + 1
         elif word[position] == "2":
             counter = counter - 1
-            word[position] = (":two:")
+            word[position] = (":two: ")
             position = position + 1
         elif word[position] == "3":
             counter = counter - 1
-            word[position] = (":three:")
+            word[position] = (":three: ")
             position = position + 1
         elif word[position] == "4":
             counter = counter - 1
-            word[position] = (":four:")
+            word[position] = (":four: ")
             position = position + 1
         elif word[position] == "5":
             counter = counter - 1
-            word[position] = (":five:")
+            word[position] = (":five: ")
             position = position + 1
         elif word[position] == "6":
             counter = counter - 1
-            word[position] = (":six:")
+            word[position] = (":six: ")
             position = position + 1
         elif word[position] == "7":
             counter = counter - 1
-            word[position] = (":seven:")
+            word[position] = (":seven: ")
             position = position + 1
         elif word[position] == "8":
             counter = counter - 1
-            word[position] = (":eight:")
+            word[position] = (":eight: ")
             position = position + 1
         elif word[position] == "9":
             counter = counter - 1
-            word[position] = (":nine:")
+            word[position] = (":nine: ")
             position = position + 1
         elif word[position] == "0":
             counter = counter - 1
-            word[position] = (":zero:")
+            word[position] = (":zero: ")
+            position = position + 1
+        elif word[position] == ";":
+            counter = counter - 1
+            word[position] = (";")
+            position = position + 1
+        elif word[position] == ",":
+            counter = counter - 1
+            word[position] = (",")
+            position = position + 1
+        elif word[position] == ".":
+            counter = counter - 1
+            word[position] = (".")
+            position = position + 1
+        elif word[position] == "?":
+            counter = counter - 1
+            word[position] == ("?")
             position = position + 1
         else:
             counter = counter - 1
@@ -88,4 +109,3 @@ b = Button(master, text="Convert to Regional Indicators", width=30, command=call
 b.pack()
 
 mainloop()
-
