7b9e772418b118fc9a914ae9661084838c5ae31d
Aukiha <aukihat@gmail.com>
2017-04-21 18:31:54 -0400

    Update script
    Changed syntax for better readability.

--- Regional Indicators.py
+++ Regional Indicators.py
@@ -22,75 +22,91 @@ def callback():
 #Checks what each value in the entry is, then converts accordingly
     while counter > 0:
         if word[position] == " ":
-            counter = counter - 1
+            counter -= 1
             word[position] = (" ")
             temp = word[position]
-            position = position + 1
+            position += 1
         elif word[position] == "b":
-            counter = counter - 1
+            counter -= 1
             word[position] = (":b: ")
-            position = position + 1
+            position += 1
         elif word[position] == "1":
-            counter = counter - 1
+            counter -= 1
             word[position] = (":one: ")
-            position = position + 1
+            position += 1
         elif word[position] == "2":
-            counter = counter - 1
+            counter -= 1
             word[position] = (":two: ")
-            position = position + 1
+            position += 1
         elif word[position] == "3":
-            counter = counter - 1
+            counter -= 1
             word[position] = (":three: ")
-            position = position + 1
+            position += 1
         elif word[position] == "4":
-            counter = counter - 1
+            counter -= 1
             word[position] = (":four: ")
-            position = position + 1
+            position += 1
         elif word[position] == "5":
-            counter = counter - 1
+            counter -= 1
             word[position] = (":five: ")
-            position = position + 1
+            position += 1
         elif word[position] == "6":
-            counter = counter - 1
+            counter -= 1
             word[position] = (":six: ")
-            position = position + 1
+            position += 1
         elif word[position] == "7":
-            counter = counter - 1
+            counter -= 1
             word[position] = (":seven: ")
-            position = position + 1
+            position += 1
         elif word[position] == "8":
-            counter = counter - 1
+            counter -= 1
             word[position] = (":eight: ")
-            position = position + 1
+            position += 1
         elif word[position] == "9":
-            counter = counter - 1
+            counter -= 1
             word[position] = (":nine: ")
-            position = position + 1
+            position += 1
         elif word[position] == "0":
-            counter = counter - 1
+            counter -= 1
             word[position] = (":zero: ")
-            position = position + 1
+            position += 1
         elif word[position] == ";":
-            counter = counter - 1
+            counter -= 1
             word[position] = (";")
-            position = position + 1
+            position += 1
         elif word[position] == ",":
-            counter = counter - 1
+            counter -= 1
             word[position] = (",")
-            position = position + 1
+            position += 1
         elif word[position] == ".":
-            counter = counter - 1
+            counter -= 1
             word[position] = (".")
-            position = position + 1
+            position += 1
         elif word[position] == "?":
-            counter = counter - 1
+            counter -= 1
             word[position] == ("?")
-            position = position + 1
+            position += 1
+        elif word[position] == "!":
+            counter -= 1
+            word[position] == ("!")
+            position += 1
+        elif word[position] == "-":
+            counter -= 1
+            word[position] == ("-")
+            position += 1
+        elif word[position] == "$":
+            counter -= 1
+            word[position] == ("-")
+            position += 1
+        elif word[position] == "&":
+            counter -= 1
+            word[position] == ("&")
+            position += 1
         else:
-            counter = counter - 1
+            counter -= 1
             temp = word[position]
             word[position] = (":regional_indicator_" + temp + ": ")
-            position = position + 1
+            position += 1
     counter =- 1
 
 #Joins together the converted words
@@ -102,7 +118,7 @@ def callback():
 l = Label(master, text="Enter text, hit convert, and enjoy your meme")
 l.pack()
 
-d = Label(master, text="Letters and numbers only.")
+d = Label(master, text="Discord has a 2,000 character limit, so keep it short.")
 d.pack()
 
 #Convert to Regional Indicators button
